bd3e53409c0196dd4a6cf659e53bcd6a0e1167b8
unknown <a.guran@AGURAN2.groupe-llp.com>
2013-07-26 13:37:07 +0200

    angular resource dont work

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,10 @@
 class StationsController < ApplicationController
+
+  def index
+    render json: Station.all
+  end
+
+  def show
+    render json: Station.find(params[:id]).to_json(:include => :entries)
+  end
 end
